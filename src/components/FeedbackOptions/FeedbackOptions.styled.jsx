aafb81ffcf58a0e460bf3e9ab9e249045a982a84
AndriiTkac4ov <106906924+AndriiTkac4ov@users.noreply.github.com>
2022-12-30 11:05:25 +0200

    fix font sizes

--- src/components/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -7,14 +7,14 @@ export const FeedbackButton = styled.button`
     border-radius: 4px;
     border: 2px solid palevioletred;
     color: palevioletred;
-    font-size: 16px;
+    font-size: ${props => props.theme.spacing(2)};
     font-weight: 500;
     line-height: 1.5;
     cursor: pointer;
     transition: background-color 0.25s ease-in-out, color 0.25s ease-in-out, border-color 0.25s ease-in-out;
     
     &:not(:last-child){
-        margin-right: ${props => props.theme.spacing(4)};
+        margin-right: 32px;
     }
 
     &:hover,
